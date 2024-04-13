de332fb9fca2d3742859c6b022311e22882de2e4
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-04-14 01:09:14 +0200

    fix server

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,41 @@
 // .eleventy.js
 import i18n from 'eleventy-plugin-i18n';
-import  translations from './src/_data/translations.json' assert { type: 'json' };
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
-export default function (eleventyConfig) {
+const translations ={
+	"about": {
+		"de-DE": "Intro",
+		"en-GB": "Intro"
+	},
+	"skills": {
+		"de-DE": "Fähigkeiten",
+		"en-GB": "Skills"
+	},
+	"projects": {
+		"de-DE": "Projekte",
+		"en-GB": "Projects"
+	},
+	"since": {
+		"de-DE": "seit",
+		"en-GB": "since"
+	},
+	"tools": {
+		"de-DE": "Werkzeuge",
+		"en-GB": "Tools"
+	},
+	"languages": {
+		"de-DE": "Sprachen",
+		"en-GB": "Languages"
+	},
+	"frameworks": {
+		"de-DE": "Frameworks",
+		"en-GB": "Frameworks"
+	}
+}
+
+
+export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(i18n, {
     translations,
     fallbackLocales: {
