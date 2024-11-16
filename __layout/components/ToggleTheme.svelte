8ec68f0bed6380be601c6da2d63ecb39e3039575
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-11-16 20:42:17 +0100

    check dark screen faster, to avoid glitch

--- __layout/components/ToggleTheme.svelte
+++ __layout/components/ToggleTheme.svelte
@@ -1,16 +1,7 @@
 <script lang="ts">
-    const darkLightModeAction: Action = (node) => {
-        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-        if (
-            localStorage.theme === "dark" ||
-            (!("theme" in localStorage) &&
-                window.matchMedia("(prefers-color-scheme: dark)").matches)
-        ) {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
+    import type { Action } from "svelte/action";
 
+    const darkLightModeAction: Action = (node) => {
         const toggle = () => {
             document.documentElement.classList.toggle("dark");
             localStorage.theme = document.documentElement.classList.contains(
@@ -29,6 +20,21 @@
         });
     };
 </script>
+
+<svelte:head>
+    <script type="text/javascript">
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (
+            localStorage.theme === "dark" ||
+            (!("theme" in localStorage) &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    </script>
+</svelte:head>
 
 <button aria-label="button" use:darkLightModeAction>
     <i class="ri-moon-fill dark:hidden block"></i>
