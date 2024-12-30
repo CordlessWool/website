4559e3f551d83aba782f61079f1e9380ff0bbd34
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-12-30 18:29:55 +0100

    make subtitles posible

--- __layout/components/Card.svelte
+++ __layout/components/Card.svelte
@@ -2,17 +2,30 @@
     import type { Snippet } from "svelte";
 
   type Props = {
-    class: string;
+    class?: string;
+    title?: string;
+    sub?: string;
     price?: string;
+
     actions?: Snippet
     children: Snippet;
   }
 
-  const { actions, children, price, ...props }: Props = $props();
+  const { actions, children, price, title, sub, ...props }: Props = $props();
 </script>
 
-<section class="markdown {props.class}">
-    <div>
+<section {...props}>
+    <div class="markdown">
+        {#if title}
+        <header>
+
+            <h2>{title}</h2>
+            {#if sub}
+                <small class="text-sm">{sub}</small>
+            {/if}
+            </header>
+
+        {/if}
     {@render children()}
     </div>
     {#if price}
@@ -21,7 +34,7 @@
         </div>
     {/if}
     {#if actions}
-        <div>
+        <div class="action">
             {@render actions()}
         </div>
     {/if}
@@ -31,19 +44,40 @@
     section {
         @apply grid gap-7;
         grid-template-rows: 1fr auto;
-        @apply rounded-md border-2 border-solid px-3 py-7;
+        @apply rounded-md border-2 border-solid px-5 py-7;
     }
 
+    header {
+        @apply mb-5;
+    }
+
+    header small {
+        @apply block pl-1 italic;
+    }
+
+    header h2 {
+        @apply pb-0 mb-0;
+    }
 
     div.price {
         @apply text-2xl font-bold text-right;
         @apply my-3 mx-7;
     }
 
+    div.action {
+        @apply w-full;
+    }
+
+
     section :global(h1),
     section :global(h2),
     section :global(h3){
         @apply mt-3;
     }
+
+    .price {
+        word-spacing: 0.3rem;
+    }
+
 
 </style>
