241e5bdfedf19d1358e2f17e298f8c67b38105c0
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-04-20 00:41:37 +0200

    filter unpublished posts

--- content/en/blog/index.ts
+++ content/en/blog/index.ts
@@ -1,15 +1,17 @@
 import { collections } from "$embodi/collections?locale=en&only=blog";
 
 export const load = ({ data }) => {
-  const posts = collections.map((posts) => {
-    return {
-      title: posts.data.title,
-      description: posts.data.description,
-      url: posts.url,
-      published: posts.data.published,
-      updated: posts.data.updated,
-      tags: posts.data.tags.filter((tag) => tag !== "blog"),
-    };
-  });
+  const posts = collections
+    .filter((post) => post.data.published !== null)
+    .map((posts) => {
+      return {
+        title: posts.data.title,
+        description: posts.data.description,
+        url: posts.url,
+        published: posts.data.published,
+        updated: posts.data.updated,
+        tags: posts.data.tags.filter((tag) => tag !== "blog"),
+      };
+    });
   return { ...data, posts };
 };
