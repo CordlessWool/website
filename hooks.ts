024ea9838fb0fcf101f60d9a6f3377dbd640bb1b
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-05-07 12:20:34 +0200

    set locale is not necassery, will taken from url

--- hooks.ts
+++
@@ -1,7 +0,0 @@
-import { setLocale } from "./__layout/lib/paraglide/runtime.js";
-
-import type { RenderHook } from "embodi";
-
-export const render: RenderHook = ({ data }) => {
-  setLocale(data.locale);
-};
