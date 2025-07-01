48d8d4bd14c9a140104b3d32a79c494ea1bca327
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-07-01 20:54:44 +0200

    update embodi to version 13 to get more access to markdown plugin

--- .embodi.js
+++ .embodi.js
@@ -1,10 +1,12 @@
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import embodiMarkdown from "@embodi/markdown";
 import tailwindcss from "@tailwindcss/vite";
 
 export default {
   source: "/content",
   plugins: [
     tailwindcss(),
+    embodiMarkdown(),
     paraglideVitePlugin({
       project: "./project.inlang", //Path to your inlang project
       outdir: "./__layout/lib/paraglide", //Where you want the generated files to be placed
