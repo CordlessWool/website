cfdeb92ef6d328de7c10d87ef90f5c3948885c36
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-08-12 02:44:03 +0200

    add text to file

--- content/donottrack.svelte
+++ content/donottrack.svelte
@@ -1,7 +1,16 @@
 <script>
     import { onMount } from "svelte";
 
+    let status = false;
+
     onMount(() => {
         localStorage.setItem("umami.disabled", 1);
+        status = localStorage.getItem("umami.disabled") === "1";
     });
 </script>
+
+{#if status}
+    <div class="donottrack">
+        <p>Umami tracking is disabled.</p>
+    </div>
+{/if}
