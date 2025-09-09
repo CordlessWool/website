3a9b20b4f74cbd248d081ba8a28cc955472d1ea7
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-09-09 17:42:45 +0200

    improve sorting of projects

--- content/en/projects/index.ts
+++ content/en/projects/index.ts
@@ -1,4 +1,5 @@
 import { collections } from "$embodi/collections?locale=en&only=project";
+import type { LoadAction } from "embodi";
 
 function getFirstParagraph(html: string): string {
   const firstParagraph = html.split("<hr>")[0];
@@ -10,13 +11,15 @@ function hasFirstParagraph(html: string): boolean {
 }
 
 export const load: LoadAction = ({ data }) => {
-  const projects = collections.map((project) => {
-    return {
-      ...project.data,
-      url: project.url,
-      html: getFirstParagraph(project.html),
-      showPageLink: hasFirstParagraph(project.html),
-    };
-  });
+  const projects = collections
+    .map((project) => {
+      return {
+        ...project.data,
+        url: project.url,
+        html: getFirstParagraph(project.html),
+        showPageLink: hasFirstParagraph(project.html),
+      };
+    })
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
   return { ...data, projects };
 };
